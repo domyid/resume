6c52ceca6946e59333ac299694d926aefcd95e15
Rolly Maulana Awangga <awangga@gmail.com>
2024-07-28 17:45:27 +0700

    update istilah

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import {getValueRadio,setInner,onClick,hide,show,getValue} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import {getHash} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
-import {get,postWithToken} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
+import {get,postJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.8/croot.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 
 
@@ -26,12 +26,18 @@ function runOnRating(){
         komentar:easymde.value()
     }
     setInner("feedback","Mohon tunggu sebentar data sedang dikirim");
-    postWithToken("https://api.do.my.id/notif/ux/postrating","login",getCookie("login"),datarating,responseFunction);
+    postJSON("https://api.do.my.id/notif/ux/postrating","login",getCookie("login"),datarating,responseFunction);
 }
 
 function responseFunction(result){
     console.log(result);
-    setInner("feedback","Risalah berhasil dikirim terima kasih. "+result.info);
+    if (response.status==200){
+        setInner("feedback","Risalah berhasil dikirim terima kasih. "+response.data.info);
+    } else{
+        console.log(result.data);
+        setInner("feedback","Terjadi kesalahan: "+response.data);
+    }
+    
 
 }
 
